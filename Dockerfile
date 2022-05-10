7efff3bc378a80147c470981581a75851b2c927d
Marcelo Galas <mgalas@bside.com.mx>
2022-05-10 15:26:01 -0500

    Added bluetooth

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,13 @@ LABEL org.opencontainers.image.source https://github.com/peanutsguy/ble-monitor
 RUN apt update
 RUN apt upgrade -y
 RUN apt autoremove -y
-RUN apt install -y git mosquitto mosquitto-clients bluez bluez-hcidump bc
+RUN apt install -y git mosquitto mosquitto-clients bluez bluez-hcidump bc bluez dbus sudo
+
+COPY ./bluezuser.conf /etc/dbus-1/system.d/
+RUN useradd -m bluezuser \
+ && adduser bluezuser sudo \
+ && passwd -d bluezuser
+USER bluezuser
 
 RUN mkdir -p /app && cd /app
 WORKDIR /app
--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+sudo service dbus start
+sudo service bluetooth start
+
+# wait for startup of services
+msg="Waiting for services to start..."
+time=0
+echo -n $msg
+while [[ "$(pidof start-stop-daemon)" != "" ]]; do
+    sleep 1
+    time=$((time + 1))
+    echo -en "\r$msg $time s"
+done
+echo -e "\r$msg done! (in $time s)"
+
+# reset bluetooth adapter by restarting it
+sudo hciconfig hci0 down
+sudo hciconfig hci0 up
+
 # ----------------------------------------------------------------------------------------
 # GENERAL INFORMATION
 # ----------------------------------------------------------------------------------------
